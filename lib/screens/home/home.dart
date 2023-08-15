37f2df87b29fa422f6e87fb057df6dd11c729dc7
yasser <y.laachir.m@gmail.com>
2023-08-15 20:36:38 +0100

    Modified the home menu
    Added icons and text that reflect the real functionality of the app

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
-import '../settings/settings.dart'; // Import your settings page
+import '../settings/settings.dart';
+import '../authenticate/sign_in.dart'; // Import your login page
 
 class Home extends StatelessWidget {
+
+  void showNotificationsMenu(BuildContext context) {
+    // Add your code to show the notifications menu here
+  }
+
+  void logout(BuildContext context) {
+    // Add your logout logic here
+    // For example, you can clear user authentication and navigate to the login page
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => SignIn()), // Navigate to login page
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('EduPulse', style: TextStyle(fontSize: 24)),
         actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.notifications),
+            onSelected: (value) {
+              // Add your logic for handling menu item selection
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: Text('Notification 1'),
+                  value: 1,
+                ),
+                PopupMenuItem(
+                  child: Text('Notification 2'),
+                  value: 2,
+                ),
+                // Add more items here
+              ];
+            },
+          ),
           IconButton(
             icon: Icon(Icons.settings),
             onPressed: () {
@@ -39,38 +73,31 @@ class Home extends StatelessWidget {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.ac_unit),
-              title: Text('Option 1'),
+              leading: Icon(Icons.account_circle), // Use the appropriate icon for Account
+              title: Text('Compte'),
               onTap: () {
-                // Add your logic for Option 1
+                // Add your logic for Account
               },
             ),
             ListTile(
-              leading: Icon(Icons.access_alarm),
-              title: Text('Option 2'),
+              leading: Icon(Icons.description), // Use the appropriate icon for Propositions
+              title: Text('Propositions'),
               onTap: () {
-                // Add your logic for Option 2
+                // Add your logic for Propositions
               },
             ),
             ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text('Option 3'),
+              leading: Icon(Icons.how_to_vote), // Use the appropriate icon for Voting
+              title: Text('Vote'),
               onTap: () {
-                // Add your logic for Option 3
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.accessibility),
-              title: Text('Option 4'),
-              onTap: () {
-                // Add your logic for Option 4
+                // Add your logic for Voting
               },
             ),
             ListTile(
               leading: Icon(Icons.exit_to_app),
               title: Text('Déconnexion'),
               onTap: () {
-                // Add your logic for logout
+                logout(context); // Call the logout function
               },
             ),
           ],
