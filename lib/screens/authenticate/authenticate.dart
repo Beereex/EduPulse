2fe87acc18b904baa338035cc69095b322c933cc
Yasser Laachir <Yasser.Laachir@men.gov.ma>
2023-08-15 12:53:13 +0100

    included provider package

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -11,7 +11,7 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("authenticate"),
+      body: Text("Auth"),
     );
   }
 }
