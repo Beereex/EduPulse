6c1b340f6126e38049af0106c8f55027dd8b5183
yasser <y.laachir.m@gmail.com>
2023-08-18 10:56:38 +0100

    test settings page

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,14 +1,70 @@
 import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool _isDarkTheme = true; // Default to dark theme
+  String _selectedLanguage = 'English'; // Default language
+
+  // List of supported languages
+  List<String> _languages = ['English', 'Français'];
+
+  void _toggleTheme(bool newValue) {
+    setState(() {
+      _isDarkTheme = newValue;
+      // You can save the user's theme preference using shared preferences
+    });
+  }
+
+  void _changeLanguage(String? newLanguage) {
+    if (newLanguage != null) {
+      setState(() {
+        _selectedLanguage = newLanguage;
+        // You can change the app's language using localization
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Paramètres'),
       ),
-      body: Center(
-        child: Text('Ceci est la page des paramètres (vide pour le moment)'),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Apparence',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SwitchListTile(
+              title: Text('Thème sombre'),
+              value: _isDarkTheme,
+              onChanged: _toggleTheme,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Langue',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            DropdownButton<String>(
+              value: _selectedLanguage,
+              onChanged: _changeLanguage,
+              items: _languages.map((String language) {
+                return DropdownMenuItem<String>(
+                  value: language,
+                  child: Text(language),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
