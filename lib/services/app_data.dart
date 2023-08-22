85a3cfb55c9ddfe704e2df05b80ec5e1bfa2d71c
Yasser Laachir <Yasser.Laachir@men.gov.ma>
2023-08-22 07:35:54 +0100

    pre-merge-pull

--- lib/services/app_data.dart
+++ lib/services/app_data.dart
@@ -4,11 +4,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AppData {
   static final AppData _instance = AppData._privateConstructor();
+
   AppData._privateConstructor();
+
   static AppData get instance => _instance;
 
   User? currentUser;
 
   Future<void> setCurrentUser(User user) async {
     currentUser = user;
+  }
 }
